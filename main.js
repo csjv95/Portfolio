6f7b6f12e75899687c7b981eea722a5ce992b089
csjv95 <csjv95@gmail.com>
2020-07-21 20:31:10 +0900

    docs : js => show up button when scrolling down and button hover

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // Make navbar transparent when it is on the
-const navbar = document.getElementById('navbar');
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  if (window,scrollY > navbarHeight) {
+  if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
@@ -34,6 +34,18 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - (window.scrollY / homeHeight);
 });
+
+// show up-btn when scrolling down
+const upBtn = document.querySelector('.up-btn');
+document.addEventListener('scroll', ()=> {
+  if(window.scrollY > homeHeight) {
+    upBtn.classList.add('visible');
+  }else {
+    upBtn.classList.remove('visible');
+  }
+});
+
+
 
 //scrollIntoView function
 const scrollIntoView = (selector) => {
